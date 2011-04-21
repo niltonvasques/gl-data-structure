a5b3610a09244c3f9de4298dcc07b421a5967608
nilton.vasques@gmail.com <nilton.vasques@gmail.com@325581f6-8de5-d20b-af7e-82cdbc739e12>
2011-04-21 09:50:06 +0000

    Criado a classe SquareShape para representar a forma de quadrado.

---
+++ GLDataStructures/GLDataStructures/SquareShape.h
@@ -0,0 +1,34 @@
+/*-----------------------------------------------------------------------------
+  Universidade Católica do Salvador - UCSAL
+  Disciplina - Organização de Arquivos
+  Aluno - Nilton Vasques Carvalho Junior
+  Autor: Nilton Vasques
+  Data do ínicio da implementação: 18/04/2011
+  Data da última modificação: 19/04/2011
+
+  Implementação da forma geométrica triângulo em OPENGL usando Orientação Objetos
+  para encapsular as rotinas da biblioteca.
+-----------------------------------------------------------------------------*/
+#ifndef SQUARE_SHAPE_H
+#define SQUARE_SHAPE_H
+
+#include <GL/glut.h>
+#include "Shape.h"
+#include "color.h"
+#include "Point.h"
+
+class SquareShape :
+	public Shape
+{
+
+public:
+	SquareShape(Point x_,unsigned int width, unsigned int height , Color color = Color(255, 0, 0));
+	virtual ~SquareShape(void);
+
+private:
+	virtual void Draw();
+	Point a,b,c;
+	Color color;
+
+};
+#endif /*Fim da classe de desenho de formas */
