b9aa47938faa90cf190d74f1863f9cf80f78e47c
nilton.vasques@gmail.com <nilton.vasques@gmail.com@325581f6-8de5-d20b-af7e-82cdbc739e12>
2011-05-12 09:16:46 +0000

    Criado um método que habilita o antiAlias na string.

--- GLDataStructures/GLDataStructures/StringShape.cpp
+++ GLDataStructures/GLDataStructures/StringShape.cpp
@@ -1,9 +1,12 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "StringShape.h"
 
-StringShape::StringShape(char *string_, Color color_, Point xyStart_):Shape(color_){
+StringShape::StringShape(char *string_, Color color_, Point xyStart_,GLuint fontSize):Shape(color_){
 	this->string = string_;
 	this->xyStart = xyStart_;
+	this->fontSizeFloat = (GLfloat) fontSize / 100;
+	this->antiAlias = false;
 }
 
 StringShape::~StringShape(){
@@ -14,6 +17,12 @@ void StringShape::setPoint(Point xyStart_){
 	this->xyStart = xyStart_;
 }
 
+void StringShape::setAntiAlias(bool enabled){
+	antiAlias = enabled;
+}
+
+
+
 void StringShape::Draw(){
 	// Limpa a janela de visualização com a cor de fundo definida
 
@@ -21,24 +30,29 @@ void StringShape::Draw(){
 	glColor4ub(color.red,color.green,color.blue,color.alpha);
 
 	//// Posiciona o texto stroke usando transformações geométricas
-	//glPushMatrix();	
-	//glTranslatef(xyStart.x,xyStart.y,0);
-	//glScalef(0.2, 0.2, 0.2); // diminui o tamanho do fonte
-	//glRotatef(45, 0,0,1); // rotaciona o texto
-	//glLineWidth(3); // define a espessura da linha
-	char *str = this->string;
-	//while(*str)
-	//	//glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,*str++);
-	//	glutStrokeCharacter(GLUT_STROKE_ROMAN,*str++); 
-	//	
+	if(antiAlias) glEnable(GL_LINE_SMOOTH);
 
-	//glPopMatrix();
+	glPushMatrix();	
+	glTranslatef(xyStart.x,xyStart.y,0);
+	printf("fontSize %f\n",fontSizeFloat);
+	glScalef(fontSizeFloat, fontSizeFloat, fontSizeFloat); // diminui o tamanho do fonte
+	//glRotatef(45, 0,0,1); // rotaciona o texto
+	glLineWidth(1); // define a espessura da linha
+	char *str = this->string;
+	while(*str)
+		//glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,*str++);
+		glutStrokeCharacter(GLUT_STROKE_ROMAN,*str++); 
+		
+
+	glPopMatrix();
+
+	glDisable(GL_LINE_SMOOTH);
 
 	//// Posição no universo onde o texto bitmap será colocado 
-	glRasterPos2f(xyStart.x,xyStart.y); 
-	str = this->string;
-	while(*str)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*str++);
+	//glRasterPos2f(xyStart.x,xyStart.y); 
+	//str = this->string;
+	//while(*str)
+	//	glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*str++);
 	//DesenhaTexto(GLUT_BITMAP_TIMES_ROMAN_24,"Bitmap");
 	//// Exibe o desenha na janela
 	//glutSwapBuffers();
--- GLDataStructures/GLDataStructures/StringShape.h
+++ GLDataStructures/GLDataStructures/StringShape.h
@@ -4,20 +4,27 @@
 #include <GL/glut.h>
 #include "Shape.h"
 
+#define DEFAULT_LINE_WIDTH 1
 class StringShape : public Shape{
 	
 public:
-	StringShape(char* string_,Color color_,Point xyStart_);
+	StringShape(char* string_,Color color_,Point xyStart_,GLuint fontSize = 10);
 	virtual ~StringShape();
 
 	virtual void Draw();
 
 	void setPoint(Point xyStart_);
 
+	void setAntiAlias(bool enabled);
 
+enum LineWidth{
+	TESTE,TESTE2
+	};
 private: 
 	char* string;
 	Point xyStart;
+	GLfloat fontSizeFloat;
+	bool antiAlias;
 	
 };
 
