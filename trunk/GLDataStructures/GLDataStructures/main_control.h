8087aa0044ed5f3fdb939cc628c6bcb2a4ee1f8c
nilton.vasques@gmail.com <nilton.vasques@gmail.com@325581f6-8de5-d20b-af7e-82cdbc739e12>
2011-05-12 09:19:07 +0000

    Refatora o main, e movido os métodos que gerenciam qual estrutura vai ser desenhada por vez para um arquivo especifico. O main_control, futuramente seria interessante revisar esse main_controller.

---
+++ trunk/GLDataStructures/GLDataStructures/main_control.h
@@ -0,0 +1,15 @@
+#ifndef STRUC_DISP_CONTROL_H
+#define STRUC_DISP_CONTROL_H
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void stackStartControl();
+
+void queueStartControl();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
