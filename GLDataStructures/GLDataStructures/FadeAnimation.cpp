1af28cba5057caad0b4abb9ea06eed1cee2a2ca0
nilton.vasques@gmail.com <nilton.vasques@gmail.com@325581f6-8de5-d20b-af7e-82cdbc739e12>
2011-05-13 15:39:10 +0000

    //TODO: Implementar AnimationManager

--- GLDataStructures/GLDataStructures/FadeAnimation.cpp
+++ GLDataStructures/GLDataStructures/FadeAnimation.cpp
@@ -9,13 +9,13 @@ FadeAnimation::~FadeAnimation(){
 
 }
 
-
+//Repensar como vai ficar o animation e implementar o ticketCount...
 void FadeAnimation::start(){
 		Rect rect = square->getRect();
 		CDisplay::getInstance()->addShape(square);
 		for(int i = 0; rect.width > 0; i++,sleep(30)){
 			square->setRect(Rect(rect.x+1,rect.y,rect.width-2,rect.height-2));
-			CDisplay::getInstance()->redraw();
+			//CDisplay::getInstance()->redraw();
 			rect = square->getRect();
 			printf("rect width %d\n",rect.width);
 		}
